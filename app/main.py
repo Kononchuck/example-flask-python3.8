30ad674b2f1f0628553a68bab99e77e7534d7170
Kononchuck <7735440@gmail.com>
2022-12-22 07:43:45 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World from Flask in a uWSGI Nginx Docker container with \
+     Python 3.8 (from the example template)"
+
+@app.route("/user")
+def hello_den():
+    return "Hello Den!"
+
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host="0.0.0.0", debug=True, port=80)
+
